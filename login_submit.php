40f5de7494a934f87ac3c101c9a3193b56c1cb43
Md Rehan <mdrehan4all@gmail.com>
2024-04-26 23:42:50 +0530

    token bug fixed

--- login_submit.php
+++ login_submit.php
@@ -5,6 +5,7 @@
         $email = trim($_POST['email']);
         $password = trim($_POST['password']);
 
+        $id='';
         $logged = 0;
         $sql = "SELECT * FROM admin WHERE email = '{$email}'";
         $result = $conn->query($sql);
@@ -21,8 +22,13 @@
         }
 
         if($logged == 1){
+            // UPDATE TOKEN
+            $token = $id .'-'. hash('md5', rand(100000, 999999));
+            $base64token = base64_encode($token);
+            $sql ="UPDATE admin SET token = '{$base64token}' WHERE email = '{$email}'";
+            if($conn->query($sql)){}
             $expire = time() + 60*60*24*30;
-            setcookie("token", "abc", $expire);
+            setcookie("token", "$base64token", $expire);
             echo '<meta http-equiv="refresh" content="0; url=index.php">';
         }else{
             echo '<meta http-equiv="refresh" content="0; url=login.php?msg=Something went Wrong">';
