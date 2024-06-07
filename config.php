c2254768c619392d32079838b640a10ebfe8a4fe
Md Rehan <42748788+mdrehan4all@users.noreply.github.com>
2024-06-07 23:05:36 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "otpindiaclone";
+    $dbname = "dbname";
     
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
